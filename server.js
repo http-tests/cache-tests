6e743a1581a16ce0ffd2c8713e6aa0526d2278cd
Mark Nottingham <mnot@mnot.net>
2018-08-02 09:57:24 +1000

    check for missing state

--- server.js
+++ server.js
@@ -75,8 +75,13 @@ function handleConfig (pathSegs, request, response) {
 var stash = new Map()
 function handleState (pathSegs, request, response) {
   var uuid = pathSegs[0]
+  var state = stash.get(uuid)
+  if (state === undefined) {
+    sendResponse(response, 404, `State not found for ${uuid}`)
+    return
+  }
   response.setHeader('Content-Type', 'text/plain')
-  response.end(JSON.stringify(stash.get(uuid)))
+  response.end(JSON.stringify(state))
 }
 
 function handleXhr (pathSegs, request, response) {
