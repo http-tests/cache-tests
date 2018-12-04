ab698db90eb25195b56e88d404a78161eabb2435
Mark Nottingham <mnot@mnot.net>
2018-12-04 11:23:54 +1100

    Start browsers

---
+++ results/index.mjs
@@ -0,0 +1,21 @@
+
+export default [
+  {
+    "file": "chrome.json",
+    "name": "Chrome",
+    "type": "browser",
+    "version": "70.0.3538.110"
+  },
+  {
+    "file": "firefox.json",
+    "name": "Firefox",
+    "type": "browser",
+    "version": "64.0b12"
+  },
+  {
+    "file": "safari.json",
+    "name": "Safari",
+    "type": "browser",
+    "version": "12.0 (14606.1.36.1.9)"
+  }
+]
