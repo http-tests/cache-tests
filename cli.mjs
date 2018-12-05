111eb6c9e8543cf7bba3b9e39385d026d99451e5
Mark Nottingham <mnot@mnot.net>
2018-12-05 15:22:40 +1100

    fix cli test array

--- cli.mjs
+++ cli.mjs
@@ -1,9 +1,9 @@
 import * as client from './client.mjs'
 import fetch from 'node-fetch'
-import baseTests from './tests/index.mjs'
+import tests from './tests/index.mjs'
 import surrogate from './tests/surrogate-control.mjs'
 
-var tests = baseTests + [surrogate]
+tests.push(surrogate)
 const baseUrl = process.env.npm_config_base || process.env.npm_package_config_base
 
 client.runTests(tests, fetch, false, baseUrl)
