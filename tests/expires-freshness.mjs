a73846c976b4dfc02f84750f8356b3d2d1b46989
Mark Nottingham <mnot@mnot.net>
2018-12-10 11:28:58 +1100

    Lots of expires tests
    For #2 #3

--- tests/expires-freshness.mjs
+++ tests/expires-freshness.mjs
@@ -71,12 +71,156 @@ export default
       ]
     },
     {
-      name: 'HTTP cache must not reuse a response with an invalid Expires',
+      name: 'HTTP cache must not reuse a response with an invalid Expires (0)',
       id: 'freshness-expires-invalid',
       requests: [
         {
           response_headers: [
             ['Expires', '0'],
+            ['Date', 0]
+          ],
+          setup: true
+        },
+        {
+          expected_type: 'not_cached'
+        }
+      ]
+    },
+    {
+      name: 'HTTP cache must not reuse a response with an invalid Expires (wrong case weekday)',
+      id: 'freshness-expires-invalid-case-weekday',
+      requests: [
+        {
+          response_headers: [
+            ['Expires', 'THU, 18 Aug 2050 02:01:18 GMT'],
+            ['Date', 0]
+          ],
+          setup: true
+        },
+        {
+          expected_type: 'not_cached'
+        }
+      ]
+    },
+    {
+      name: 'HTTP cache must not reuse a response with an invalid Expires (wrong case month)',
+      id: 'freshness-expires-invalid-case-month',
+      requests: [
+        {
+          response_headers: [
+            ['Expires', 'Thu, 18 AUG 2050 02:01:18 GMT'],
+            ['Date', 0]
+          ],
+          setup: true
+        },
+        {
+          expected_type: 'not_cached'
+        }
+      ]
+    },
+    {
+      name: 'HTTP cache must not reuse a response with an invalid Expires (UTC)',
+      id: 'freshness-expires-invalid-utc',
+      requests: [
+        {
+          response_headers: [
+            ['Expires', 'Thu, 18 Aug 2050 02:01:18 UTC'],
+            ['Date', 0]
+          ],
+          setup: true
+        },
+        {
+          expected_type: 'not_cached'
+        }
+      ]
+    },
+    {
+      name: 'HTTP cache must not reuse a response with an invalid Expires (two-digit year)',
+      id: 'freshness-expires-invalid-2-digit-year',
+      requests: [
+        {
+          response_headers: [
+            ['Expires', 'Thu, 18 Aug 50 02:01:18 GMT'],
+            ['Date', 0]
+          ],
+          setup: true
+        },
+        {
+          expected_type: 'not_cached'
+        }
+      ]
+    },
+    {
+      name: 'HTTP cache must not reuse a response with an invalid Expires (missing comma)',
+      id: 'freshness-expires-invalid-no-comma',
+      requests: [
+        {
+          response_headers: [
+            ['Expires', 'Thu 18 Aug 2050 02:01:18 GMT'],
+            ['Date', 0]
+          ],
+          setup: true
+        },
+        {
+          expected_type: 'not_cached'
+        }
+      ]
+    },
+    {
+      name: 'HTTP cache must not reuse a response with an invalid Expires (multiple spaces)',
+      id: 'freshness-expires-invalid-multiple-spaces',
+      requests: [
+        {
+          response_headers: [
+            ['Expires', 'Thu, 18  Aug  2050 02:01:18 GMT'],
+            ['Date', 0]
+          ],
+          setup: true
+        },
+        {
+          expected_type: 'not_cached'
+        }
+      ]
+    },
+    {
+      name: 'HTTP cache must not reuse a response with an invalid Expires (date dashes)',
+      id: 'freshness-expires-invalid-date-dashes',
+      requests: [
+        {
+          response_headers: [
+            ['Expires', 'Thu, 18-Aug-2050 02:01:18 GMT'],
+            ['Date', 0]
+          ],
+          setup: true
+        },
+        {
+          expected_type: 'not_cached'
+        }
+      ]
+    },
+    {
+      name: 'HTTP cache must not reuse a response with an invalid Expires (time periods)',
+      id: 'freshness-expires-invalid-time-periods',
+      requests: [
+        {
+          response_headers: [
+            ['Expires', 'Thu, 18 Aug 2050 02.01.18 GMT'],
+            ['Date', 0]
+          ],
+          setup: true
+        },
+        {
+          expected_type: 'not_cached'
+        }
+      ]
+    },
+    {
+      name: 'HTTP cache must not reuse a response with an invalid Expires (1-digit hour)',
+      id: 'freshness-expires-invalid-1-digit-hour',
+      requests: [
+        {
+          response_headers: [
+            ['Expires', 'Thu, 18 Aug 2050 2:01:18 GMT'],
             ['Date', 0]
           ],
           setup: true
