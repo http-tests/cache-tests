621cd48342905997f63ab853f9a12819ace6f810
Mark Nottingham <mnot@mnot.net>
2019-09-04 16:49:05 +1000

    test age parsing

---
+++ tests/age-parse.mjs
@@ -0,0 +1,109 @@
+export default
+
+{
+  name: 'Age Parsing',
+  id: 'age-parse',
+  description: 'These tests check how implementations parse the `Age` response header. They are not conformance tests because error handling is not clearly specified; rather, they are being used to gather information as input to spec revisions.',
+  tests: [
+    {
+      name: 'Does HTTP cache reuse a response when the `Age` header is non-numeric?',
+      id: 'age-parse-nonnumeric',
+      kind: 'check',
+      depends_on: ['freshness-max-age-age'],
+      requests: [
+        {
+          response_headers: [
+            ['Date', 0],
+            ['Cache-Control', 'max-age=3600'],
+            ['Age', 'abc']
+          ],
+          setup: true,
+          pause_after: true
+        },
+        {
+          expected_type: 'cached'
+        }
+      ]
+    },
+    {
+      name: 'Does HTTP cache reuse a response when the `Age` header is negative?',
+      id: 'age-parse-negative',
+      kind: 'check',
+      depends_on: ['freshness-max-age-age'],
+      requests: [
+        {
+          response_headers: [
+            ['Date', 0],
+            ['Cache-Control', 'max-age=3600'],
+            ['Age', '-7200']
+          ],
+          setup: true,
+          pause_after: true
+        },
+        {
+          expected_type: 'cached'
+        }
+      ]
+    },
+    {
+      name: 'Does HTTP cache reuse a response when the `Age` header is a float?',
+      id: 'age-parse-float',
+      kind: 'check',
+      depends_on: ['freshness-max-age-age'],
+      requests: [
+        {
+          response_headers: [
+            ['Date', 0],
+            ['Cache-Control', 'max-age=3600'],
+            ['Age', '7200.0']
+          ],
+          setup: true,
+          pause_after: true
+        },
+        {
+          expected_type: 'cached'
+        }
+      ]
+    },
+    {
+      name: 'Does HTTP cache reuse an aged response when the `Age` header has ",0" appended?',
+      id: 'age-parse-suffix',
+      kind: 'check',
+      depends_on: ['freshness-max-age-age'],
+      requests: [
+        {
+          response_headers: [
+            ['Date', 0],
+            ['Cache-Control', 'max-age=3600'],
+            ['Age', '7200,0']
+          ],
+          setup: true,
+          pause_after: true
+        },
+        {
+          expected_type: 'cached'
+        }
+      ]
+    },
+    {
+      name: 'Does HTTP cache reuse an aged response when the `Age` header has "0," prepended?',
+      id: 'age-parse-prefix',
+      kind: 'check',
+      depends_on: ['freshness-max-age-age'],
+      requests: [
+        {
+          response_headers: [
+            ['Date', 0],
+            ['Cache-Control', 'max-age=3600'],
+            ['Age', '0,7200']
+          ],
+          setup: true,
+          pause_after: true
+        },
+        {
+          expected_type: 'cached'
+        }
+      ]
+    }
+  ]
+}
--- tests/index.mjs
+++ tests/index.mjs
@@ -2,6 +2,7 @@ import ccParse from './cc-parse.mjs'
 import ccRequest from './cc-request.mjs'
 import ccResponse from './cc-response.mjs'
 import ccFreshness from './cc-freshness.mjs'
+import ageParse from './age-parse.mjs'
 import pragma from './pragma.mjs'
 import expiresParse from './expires-parse.mjs'
 import expires from './expires-freshness.mjs'
@@ -15,4 +16,4 @@ import invalidation from './invalidation.mjs'
 import partial from './partial.mjs'
 import other from './other.mjs'
 
-export default [ccFreshness, ccParse, expires, expiresParse, ccResponse, heuristic, statuses, ccRequest, pragma, vary, conditional, headers, update304, invalidation, partial, other]
+export default [ccFreshness, ccParse, ageParse, expires, expiresParse, ccResponse, heuristic, statuses, ccRequest, pragma, vary, conditional, headers, update304, invalidation, partial, other]
