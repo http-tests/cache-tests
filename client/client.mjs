896dfa43eddfe3d8a7c7d7485babecb01450f6a2
Mark Nottingham <mnot@mnot.net>
2020-09-23 20:44:40 +1000

    factor out some client utils

--- client/client.mjs
+++ client/client.mjs
@@ -4,6 +4,7 @@
 */
 
 import * as utils from '../utils.mjs'
+import { pause, makeTestUrl, putTestConfig, getServerState, setupCheck } from './utils.mjs'
 import * as defines from './defines.mjs'
 import * as config from './config.mjs'
 const assert = utils.assert
@@ -318,61 +319,4 @@ function checkRequests (requests, responses, testState) {
       })
     }
   }
-}
-
-function pause () {
-  return new Promise(function (resolve, reject) {
-    setTimeout(() => {
-      return resolve()
-    }, 3000)
-  })
-}
-
-function makeTestUrl (uuid, reqConfig) {
-  var extra = ''
-  if ('filename' in reqConfig) {
-    extra += `/${reqConfig.filename}`
-  }
-  if ('query_arg' in reqConfig) {
-    extra += `?${reqConfig.query_arg}`
-  }
-  return `${config.baseUrl}/test/${uuid}${extra}`
-}
-
-const uninterestingHeaders = new Set(['date', 'expires', 'last-modified', 'content-length', 'content-type', 'connection', 'content-language', 'vary', 'mime-version'])
-
-function putTestConfig (uuid, requests) {
-  var init = {
-    method: 'PUT',
-    headers: [['content-type', 'application/json']],
-    body: JSON.stringify(requests)
-  }
-  return config.fetch(`${config.baseUrl}/config/${uuid}`, init)
-    .then(response => {
-      if (response.status !== 201) {
-        var headers = ''
-        response.headers.forEach((hvalue, hname) => { // for some reason, node-fetch reverses these
-          if (!uninterestingHeaders.has(hname.toLowerCase())) {
-            headers += `${hname}: ${hvalue}    `
-          }
-        })
-        throw new utils.SetupError({ message: `PUT config resulted in ${response.status} ${response.statusText} - ${headers}` })
-      }
-    })
-}
-
-function getServerState (uuid) {
-  return config.fetch(`${config.baseUrl}/state/${uuid}`)
-    .then(response => {
-      if (response.status === 200) {
-        return response.text()
-      }
-    }).then(text => {
-      if (text === undefined) return []
-      return JSON.parse(text)
-    })
-}
-
-function setupCheck (reqConfig, memberName) {
-  return reqConfig.setup === true || ('setup_tests' in reqConfig && reqConfig.setup_tests.indexOf(memberName) > -1)
 }
---
+++ client/utils.mjs
@@ -0,0 +1,60 @@
+
+import * as config from './config.mjs'
+import * as utils from '../utils.mjs'
+
+export function pause () {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      return resolve()
+    }, 3000)
+  })
+}
+
+export function makeTestUrl (uuid, reqConfig) {
+  var extra = ''
+  if ('filename' in reqConfig) {
+    extra += `/${reqConfig.filename}`
+  }
+  if ('query_arg' in reqConfig) {
+    extra += `?${reqConfig.query_arg}`
+  }
+  return `${config.baseUrl}/test/${uuid}${extra}`
+}
+
+const uninterestingHeaders = new Set(['date', 'expires', 'last-modified', 'content-length', 'content-type', 'connection', 'content-language', 'vary', 'mime-version'])
+
+export function putTestConfig (uuid, requests) {
+  var init = {
+    method: 'PUT',
+    headers: [['content-type', 'application/json']],
+    body: JSON.stringify(requests)
+  }
+  return config.fetch(`${config.baseUrl}/config/${uuid}`, init)
+    .then(response => {
+      if (response.status !== 201) {
+        var headers = ''
+        response.headers.forEach((hvalue, hname) => { // for some reason, node-fetch reverses these
+          if (!uninterestingHeaders.has(hname.toLowerCase())) {
+            headers += `${hname}: ${hvalue}    `
+          }
+        })
+        throw new utils.SetupError({ message: `PUT config resulted in ${response.status} ${response.statusText} - ${headers}` })
+      }
+    })
+}
+
+export function getServerState (uuid) {
+  return config.fetch(`${config.baseUrl}/state/${uuid}`)
+    .then(response => {
+      if (response.status === 200) {
+        return response.text()
+      }
+    }).then(text => {
+      if (text === undefined) return []
+      return JSON.parse(text)
+    })
+}
+
+export function setupCheck (reqConfig, memberName) {
+  return reqConfig.setup === true || ('setup_tests' in reqConfig && reqConfig.setup_tests.indexOf(memberName) > -1)
+}
