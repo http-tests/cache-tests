8a569eaa9224983615c17bfcbb3c45e368adf06f
Mark Nottingham <mnot@mnot.net>
2020-10-09 18:21:35 +1100

    Add JSON schema validation
    For #75

--- export.mjs
+++ export.mjs
@@ -1,6 +1,18 @@
+import fs from 'fs'
+
+import Ajv from 'ajv'
+
 import tests from './tests/index.mjs'
 import surrogate from './tests/surrogate-control.mjs'
 
 tests.push(surrogate)
 
-console.log(JSON.stringify(tests, null, 2))
+if (process.argv[2] === 'validate') {
+  var ajv = new Ajv()
+  var schema = JSON.parse(fs.readFileSync('lib/testsuite-schema.json', 'utf8'))
+  var validate = ajv.compile(schema)
+  var valid = validate(tests)
+  if (!valid) console.log(validate.errors)
+} else {
+  console.log(JSON.stringify(tests, null, 2))
+}
