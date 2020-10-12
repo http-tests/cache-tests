026d2c57b48e4858f57d12af71551ddcd1b26b75
Mark Nottingham <mnot@mnot.net>
2020-10-12 17:25:52 +1100

    check authorization
    Fixes #80

--- tests/other.mjs
+++ tests/other.mjs
@@ -92,6 +92,56 @@ export default
       ]
     },
     {
+      name: 'HTTP shared cache must not reuse a response to a request that contained `Authorization`, even with explicit freshness',
+      id: 'other-authorization',
+      browser_skip: true,
+      requests: [
+        {
+          request_headers: [
+            ['Authorization', 'FOO']
+          ],
+          expected_request_headers: [
+            ['Authorization', 'FOO']
+          ],
+          response_headers: [
+            ['Expires', 30 * 24 * 60 * 60],
+            ['Date', 0]
+          ],
+          pause_after: true,
+          setup: true
+        },
+        {
+          expected_type: 'not_cached'
+        }
+      ]
+    },
+    {
+      name: 'An optimal HTTP shared cache reuses a response to a request that contained `Authorization`, if it has `Cache-Control: public`',
+      id: 'other-authorization-public',
+      kind: 'optimal',
+      browser_skip: true,
+      depends_on: ['other-authorization'],
+      requests: [
+        {
+          request_headers: [
+            ['Authorization', 'FOO']
+          ],
+          expected_request_headers: [
+            ['Authorization', 'FOO']
+          ],
+          response_headers: [
+            ['Cache-Control', 'max-age=3600, public'],
+            ['Date', 0]
+          ],
+          pause_after: true,
+          setup: true
+        },
+        {
+          expected_type: 'cached'
+        }
+      ]
+    },
+    {
       name: 'Different query arguments must be different cache keys',
       id: 'query-args-different',
       requests: [
