edd651020275fc59423c813345376263b5371c9c
Mark Nottingham <mnot@mnot.net>
2020-10-19 15:36:38 +1100

    add spec anchor

--- tests/heuristic-freshness.mjs
+++ tests/heuristic-freshness.mjs
@@ -88,5 +88,6 @@ export default {
   name: 'Heuristic Freshness',
   id: 'heuristic',
   description: 'These tests check whether caches are conformant and optimal in handling [heuristic freshness](https://httpwg.org/specs/rfc7234.html#heuristic.freshness). Certain status codes can be stored and reused even if they do not have an explicit freshness lifetime associated with them; these tests check to see if caches are taking advantage of that, and also make sure that other status codes are NOT cached in this way.',
+  spec_anchors: ['heuristic.freshness'],
   tests: tests
 }
