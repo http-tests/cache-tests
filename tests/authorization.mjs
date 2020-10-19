4d4c82bc47bf3fcac90ae09abbdf357fc1560411
Mark Nottingham <mnot@mnot.net>
2020-10-19 15:16:25 +1100

    Add more auth tests

--- tests/authorization.mjs
+++ tests/authorization.mjs
@@ -4,7 +4,7 @@ export default
 {
   name: 'Storing Respones to Authenticated Requests',
   id: 'auth',
-  description: 'These tests check for conformance to other requirements that apply to HTTP caches. ',
+  description: 'These tests check for conformance to other requirements that apply to HTTP caches. Note that some proxies might fail the first test because they consider themselves the origin (i.e., they\'re \'reverse proxies\')',
   spec_anchors: ['caching.authenticated.responses'],
   tests: [
     {
@@ -47,6 +47,58 @@ export default
           ],
           response_headers: [
             ['Cache-Control', 'max-age=3600, public'],
+            ['Date', 0]
+          ],
+          pause_after: true,
+          setup: true
+        },
+        {
+          expected_type: 'cached'
+        }
+      ]
+    },
+    {
+      name: 'An optimal HTTP shared cache reuses a response to a request that contained `Authorization`, if it has `Cache-Control: must-revalidate`',
+      id: 'other-authorization-must-revalidate',
+      kind: 'optimal',
+      browser_skip: true,
+      depends_on: ['other-authorization'],
+      requests: [
+        {
+          request_headers: [
+            ['Authorization', 'FOO']
+          ],
+          expected_request_headers: [
+            ['Authorization', 'FOO']
+          ],
+          response_headers: [
+            ['Cache-Control', 'max-age=3600, must-revalidate'],
+            ['Date', 0]
+          ],
+          pause_after: true,
+          setup: true
+        },
+        {
+          expected_type: 'cached'
+        }
+      ]
+    },
+    {
+      name: 'An optimal HTTP shared cache reuses a response to a request that contained `Authorization`, if it has `Cache-Control: s-maxage`',
+      id: 'other-authorization-smaxage',
+      kind: 'optimal',
+      browser_skip: true,
+      depends_on: ['other-authorization'],
+      requests: [
+        {
+          request_headers: [
+            ['Authorization', 'FOO']
+          ],
+          expected_request_headers: [
+            ['Authorization', 'FOO']
+          ],
+          response_headers: [
+            ['Cache-Control', 's-maxage=3600'],
             ['Date', 0]
           ],
           pause_after: true,
