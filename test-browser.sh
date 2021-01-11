78be726cf60905329aab4ef02a4f5bdddd105c0a
Mark Nottingham <mnot@mnot.net>
2021-01-11 14:39:37 +1100

    try firefox with --headless

--- test-browser.sh
+++ test-browser.sh
@@ -51,7 +51,7 @@ function test_browser {
   rm -f "${TARGET}"
 
   # run tests
-  open -jg -a "${BROWSER_CMD}" "${URL}"
+  open -jg -a "${BROWSER_CMD}" "${URL}" --args --headless
 
   # wait for the target to be created
   i=0
