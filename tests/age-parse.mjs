2e3495d2deed8889a4f583eb7c445bacb6f4ab9a
Mark Nottingham <mnot@mnot.net>
2021-02-17 17:58:31 +1100

    consistency

--- tests/age-parse.mjs
+++ tests/age-parse.mjs
@@ -3,7 +3,7 @@ export default
 {
   name: 'Age Parsing',
   id: 'age-parse',
-  description: 'These tests check how implementations parse the `Age` response header.',
+  description: 'These tests check how caches parse the `Age` response header.',
   spec_anchors: ['field.age', 'expiration.model'],
   tests: [
     {
