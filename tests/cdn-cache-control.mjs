070ca94e67451e68dae65c6c8cd75056389b7e61
Adam C. Foltzer <acfoltzer@acfoltzer.net>
2023-01-23 11:18:37 -0800

    Update tests/cdn-cache-control.mjs
    Co-authored-by: Mark Nottingham <mnot@mnot.net>

--- tests/cdn-cache-control.mjs
+++ tests/cdn-cache-control.mjs
@@ -161,7 +161,7 @@ export default
       ]
     },
     {
-      name: 'An optimal CDN reuses a response with a positive `CDN-Cache-Control: MaX-aGe`',
+      name: 'Does CDN reuse a response with a positive `CDN-Cache-Control: MaX-aGe`?',
       id: 'cdn-max-age-case-insensitive',
       cdn_only: true,
       kind: 'check',
