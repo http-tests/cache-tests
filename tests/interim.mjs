6a95ba9266d47a15dd63949644ed1af5abef7b3a
Marius Kleidl <marius@transloadit.com>
2025-03-12 12:32:26 +0100

    Add tests for https://github.com/http-tests/cache-tests/issues/78

--- tests/interim.mjs
+++ tests/interim.mjs
@@ -54,6 +54,71 @@ export default
           expected_type: 'cached'
         }
       ]
+    },
+    {
+      name: 'An HTTP cache should not cache non-final responses',
+      id: 'interim-not-cached',
+      browser_skip: true,
+      kind: 'required',
+      requests: [
+        {
+          interim_responses: [
+            [103, [
+              ['link', '</styles.css>; rel=preload; as=style']
+            ]]
+          ],
+          expected_interim_responses: [
+            [103, [
+              ['link', '</styles.css>; rel=preload; as=style']
+            ]]
+          ],
+          response_headers: [
+            ['Cache-Control', 'max-age=100000'],
+            ['Date', 0]
+          ],
+          pause_after: true
+        },
+        {
+          expected_type: 'cached',
+          expected_interim_responses: []
+        }
+      ]
+    },
+    {
+      name: 'An optimal HTTP cache should not store headers from non-final responses',
+      id: 'interim-no-header-reuse',
+      browser_skip: true,
+      kind: 'optimal',
+      requests: [
+        {
+          interim_responses: [
+            [103, [
+              ['link', '</styles.css>; rel=preload; as=style'],
+              ['x-my-header', 'test']
+            ]]
+          ],
+          expected_interim_responses: [
+            [103, [
+              ['link', '</styles.css>; rel=preload; as=style'],
+              ['x-my-header', 'test']
+            ]]
+          ],
+          response_headers: [
+            ['Cache-Control', 'max-age=100000'],
+            ['Date', 0]
+          ],
+          expected_response_headers_missing: [
+            'x-my-header'
+          ],
+          pause_after: true
+        },
+        {
+          expected_type: 'cached',
+          expected_response_headers_missing: [
+            'x-my-header'
+          ]
+        }
+      ]
     }
   ]
 }
